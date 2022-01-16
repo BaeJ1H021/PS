439e1a71ca61541ae264c708c5bc98f2ddc19856
BaeJ1H021 <87417773+BaeJ1H021@users.noreply.github.com>
2022-01-17 03:10:25 +0900

    Add files via upload

---
+++ 최대 유량/백준 15892 (사탕 줍는 로봇).cc
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<queue>
+#include<memory.h>
+#include<vector>
+#include<cmath>
+#include<stack>
+#include<map>
+
+#define INF 2e9
+
+using namespace std;
+
+const int MAX_N = 301;
+int n,m;
+// a -> b 가는 간선의 용량.
+int capacity[MAX_N][MAX_N];
+// a -> b 가는 간선의 유량.
+int flow[MAX_N][MAX_N];
+
+//에드몬드-카프 알고리즘.
+int networkFlow(int source, int sink)
+{
+  int totalFlow = 0;
+  while(true)
+  {
+    //어느 정점에서 유량이 흘러왔는지 저장할 배열.
+    int parent[MAX_N];
+    memset(parent,-1,sizeof(parent));
+
+    //유량을 흘려보낼 정점을 저장할 큐.
+    queue<int> q;
+
+    parent[source] = source;
+    q.push(source);
+
+    while(!q.empty() && parent[sink] == -1)
+    {
+      int cur = q.front();
+      q.pop();
+
+      for(int next=0;next<MAX_N;next++)
+      {
+        //잔여용량이 남아있고 아직 방문하지 않았다면.
+        if(capacity[cur][next]-flow[cur][next] > 0  && parent[next]==-1)
+        {
+          q.push(next);
+          parent[next] = cur;
+        }
+      }
+    }
+    //증가 경로가 없으면 종료한다.
+    if(parent[sink]==-1)
+      break;
+    //증가 경로를 통해서 얼마를 보낼지 결정.
+    int amount = INF;
+    //증가 경로의 간선 중에서 잔여 용량이 가장 작은 것을 유량으로 채택. 
+    for(int cur = sink;cur!=source;cur=parent[cur])
+      amount = min(amount,capacity[parent[cur]][cur]-flow[parent[cur]][cur]);
+    //증가 경로는 유량 증가, 역 방향 경로는 유량 감소.
+    for(int cur = sink;cur!=source;cur=parent[cur])
+    {
+      flow[parent[cur]][cur] += amount;
+      flow[cur][parent[cur]] -= amount;
+    }
+    totalFlow += amount;
+  }
+  return totalFlow;
+}
+
+int main()                             
+{ 
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  cin >> n >> m;
+  while(m-->0)
+  {
+    int a,b,w;
+    cin >> a >> b >> w;
+    
+    //양방향이니까 양쪽으로 용량 증가.
+    capacity[a][b] += w;
+    capacity[b][a] += w;
+  }
+  cout << networkFlow(1,n);
+ 
+  return 0;
+}
