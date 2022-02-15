b5f57d363720e1b0c619dd8602a15e1d60c9a0be
BaeJ1H021 <87417773+BaeJ1H021@users.noreply.github.com>
2022-02-15 21:37:43 +0900

    Add files via upload

---
+++ 이분 매칭/백준 14398 (피타고라스 수).cc
@@ -0,0 +1,107 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<queue>
+#include<memory.h>
+#include<vector>
+#include<cmath>
+#include<stack>
+#include<map>
+
+#define INF 2e9
+
+using namespace std;
+
+const int MAX = 201;
+
+// A와 B의 정점의 개수
+int n;
+// adj[i] : A[i]와 인접한 그룹 B의 정점들.
+vector<int> adj[MAX];
+// A[i], B[i] : 각 정점에 매칭된 반대편 정점 번호.
+int A[MAX], B[MAX];
+// dfs의 방문 여부.
+bool visited[MAX];
+// 홀수,짝수 담는 벡터.
+vector<int> v1, v2;
+
+// 최대공약수.
+int gcd(int n,int k)
+{
+    if (k == 0)
+        return n;
+    return gcd(k, n % k);
+}
+
+// A 그룹의 정점인 a에서 B의 매칭되지 않은 정점으로 가는 경로 탐색. 
+bool dfs(int a)
+{
+    if (visited[a])
+        return false;
+    visited[a] = true;
+    for (int i = 0; i < adj[a].size(); i++)
+    {
+        int b = adj[a][i];
+        // 반대편이 매칭되지 않았거나
+        // 매칭되어 있었지만 원래 매칭되어 있던 정점을 다른 정점과 매칭가능하다면.
+        if (B[b] == -1 || dfs(B[b]))
+        {
+            A[a] = b;
+            B[b] = a;
+            return true;
+        }
+    }
+    return false;
+}
+
+int bipartiteMatch()
+{
+    memset(A, -1, sizeof(A));
+    memset(B, -1, sizeof(B));
+    // 매칭의 수.
+    int cnt = 0;
+
+    for (int i = 0; i < v1.size(); i++)
+    {
+        memset(visited, false, sizeof(visited));
+        if (dfs(i))
+            cnt++;
+    }
+    return cnt;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int num;
+        cin >> num;
+        if (num % 2 == 1)
+            v1.push_back(num);
+        else
+            v2.push_back(num);
+    }
+
+    for (int i = 0; i < v1.size(); i++)
+    {
+        for (int j = 0; j < v2.size(); j++)
+        {
+            long long x = v1[i];
+            long long y = v2[j];
+            long long sum = x * x + y * y;
+            long long root = sqrt(sum);
+            if (gcd(x, y) == 1 && sum == root * root)
+                adj[i].push_back(j);
+        }
+    }
+
+    cout << bipartiteMatch();
+
+    return 0;
+}
